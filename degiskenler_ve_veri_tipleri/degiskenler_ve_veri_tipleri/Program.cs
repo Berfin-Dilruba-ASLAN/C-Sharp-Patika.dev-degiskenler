b937cf7140b8f684e43c8abc75e23abb7dff3b33
Berfin Dilruba ASLAN <98692629+Berfin-Dilruba-ASLAN@users.noreply.github.com>
2022-08-14 21:47:52 +0300

    Add files via upload

---
+++ degiskenler_ve_veri_tipleri/degiskenler_ve_veri_tipleri/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace degiskenler_ve_veri_tipleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String inputString;
+            inputString = Console.ReadLine();
+            Console.WriteLine("Hello, World.");
+            Console.Write(inputString);
+        }
+    }
+}
